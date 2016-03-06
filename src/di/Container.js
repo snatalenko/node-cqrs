bb81231e08262d9c3b9d9d0cd018d6f3b22aa158
Stas <stas@natalenko.com>
2016-03-06 17:59:48 +0200

    + added instance registration to DI container

--- src/di/Container.js
+++ src/di/Container.js
@@ -2,11 +2,17 @@
 
 const debug = require('debug')('cqrs:Container');
 const getClassDependencyNames = require('./getClassDependencyNames');
+const _factories = Symbol('factories');
+const _instances = Symbol('instances');
+
+function isObject(instance) {
+	return typeof instance === 'object' && instance != null && !Array.isArray(instance) && !(instance instanceof Date);
+}
 
 function createInstance(typeOrFactory, container, additionalOptions) {
 	if (typeof typeOrFactory !== 'function') throw new TypeError('typeOrFactory argument must be a Function');
 	if (!container) throw new TypeError('container argument required');
-	if (additionalOptions && (typeof additionalOptions !== 'object' || Array.isArray(additionalOptions)))
+	if (additionalOptions && !isObject(additionalOptions))
 		throw new TypeError('additionalOptions argument, when specified, must be an Object');
 
 	if (typeOrFactory.prototype) {
@@ -39,18 +45,19 @@ function createInstance(typeOrFactory, container, additionalOptions) {
 }
 
 
+
 module.exports = class Container {
 
 	get factories() {
-		return this._factories || (this._factories = []);
+		return this[_factories] || (this[_factories] = []);
 	}
 
 	get instances() {
-		return this._instances || (this._instances = {});
+		return this[_instances] || (this[_instances] = {});
 	}
 
 	/**
-	 * Registers a component in the container
+	 * Registers a type or factory in the container
 	 * @param  {Function} 	typeOrFactory	Either a constructor function or a component factor
 	 * @param  {String} 	exposeAs      	Optional component name to use for instance exposing on the container
 	 * @param  {Function} 	exposeMap     	Optional Instance -> Object-to-Expose mapping
@@ -77,6 +84,30 @@ module.exports = class Container {
 		}
 	}
 
+	/**
+	 * Registers an object instance in the container
+	 * @param  {Object} instance Object instance to register
+	 * @param  {String} exposeAs Object name to use for instance exposing on the container
+	 * @return {undefined}
+	 */
+	registerInstance(instance, exposeAs) {
+		if (!isObject(instance)) throw new TypeError('instance argument must be an Object');
+		if (typeof exposeAs !== 'string' || !exposeAs.length) throw new TypeError('exposeAs argument must be a non-empty String');
+
+		this.instances[exposeAs] = instance;
+
+		Object.defineProperty(this, exposeAs, {
+			get: function () {
+				return this.instances[exposeAs];
+			}
+		});
+	}
+
+	/**
+	 * Create instances for components that do not have lazy getters defined on the Container.
+	 * For example, event or command handlers, that are not referenced from external components.
+	 * @return {undefined}
+	 */
 	createUnexposedInstances() {
 		debug('creating unexposed instances...');
 		for (let i = 0; i < this.factories.length; i++) {
@@ -87,6 +118,10 @@ module.exports = class Container {
 		}
 	}
 
+	/**
+	 * Creates instances for all types or factories registered in the Container
+	 * @return {undefined}
+	 */
 	createAllInstances() {
 		debug('creating all instances...');
 		while (this.factories.length) {
@@ -94,6 +129,12 @@ module.exports = class Container {
 		}
 	}
 
+	/**
+	 * Creates an instance from the given type or factory using dependency injection
+	 * @param  {Function} typeOrFactory   Type or factory used to create an instance
+	 * @param  {Object} additionalOptions Additional options to append to the parameter object of the type constructor
+	 * @return {Object}                   Newly created instance
+	 */
 	createInstance(typeOrFactory, additionalOptions) {
 		debug(`creating ${typeOrFactory.name || 'unnamed'} instance...`);
 
