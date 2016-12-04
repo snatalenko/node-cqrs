fe3839185f04121d8684ebf91331825271b6eca6
Stas <stas@natalenko.com>
2016-12-04 21:29:53 +0200

    - fixed sizeOf is not a function

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,7 @@
 exports.getHandler = require('./getHandler');
 exports.passToHandler = require('./passToHandler');
 exports.passToHandlerAsync = require('./passToHandlerAsync');
-exports.sideOf = require('./sizeOf');
+exports.sizeOf = require('./sizeOf');
 exports.validateHandlers = require('./validateHandlers');
 exports.isClass = require('./isClass');
 exports.coWrap = require('./coWrap');
