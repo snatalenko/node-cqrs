5e49e4c31ce02a102c454196eeb449067f4ab1b0
Stas <stas@natalenko.com>
2018-10-29 11:38:34 +0200

    * typings and validations

--- src/Observer.js
+++ src/Observer.js
@@ -9,21 +9,25 @@ const unique = arr => [...new Set(arr)];
  *
  * @param {IObservable} observable
  * @param {IObserver} observer
- * @param {object} [options]
- * @param {string[]} [options.messageTypes]
- * @param {IMessageHandler} [options.masterHandler]
- * @param {string} [options.queueName]
+ * @param {TSubscribeOptions} [options]
  */
 function subscribe(observable, observer, options) {
-	if (typeof observable !== 'object' || !observable) throw new TypeError('observable argument must be an Object');
-	if (typeof observable.on !== 'function') throw new TypeError('observable.on must be a Function');
-	if (typeof observer !== 'object' || !observer) throw new TypeError('observer argument must be an Object');
+	if (typeof observable !== 'object' || !observable)
+		throw new TypeError('observable argument must be an Object');
+	if (typeof observable.on !== 'function')
+		throw new TypeError('observable.on must be a Function');
+	if (typeof observer !== 'object' || !observer)
+		throw new TypeError('observer argument must be an Object');
 
 	const { masterHandler, messageTypes, queueName } = options;
 	if (masterHandler && typeof masterHandler !== 'function')
 		throw new TypeError('masterHandler parameter, when provided, must be a Function');
+	if (queueName && typeof observable.queue !== 'function')
+		throw new TypeError('observable.queue, when queueName is specified, must be a Function');
 
-	const subscribeTo = messageTypes || observer.handles || Object.getPrototypeOf(observer).constructor.handles;
+	const subscribeTo = messageTypes
+		|| observer.handles
+		|| Object.getPrototypeOf(observer).constructor.handles;
 	if (!Array.isArray(subscribeTo))
 		throw new TypeError('either options.messageTypes, observer.handles or ObserverType.handles is required');
 
@@ -62,7 +66,7 @@ class Observer {
 	 * @static
 	 * @param {IObservable} observable
 	 * @param {IObserver} observer
-	 * @param {object} options
+	 * @param {TSubscribeOptions} options
 	 * @returns
 	 */
 	static subscribe(observable, observer, options) {
