8c6ead0a9b4f3feba7bbfba539082eeb0b09b9f9
Stanislav Natalenko <stas@natalenko.com>
2025-12-05 00:53:50 +0000

    Build: Update changelog titles and commit message prefixes

--- scripts/changelog/index.js
+++ scripts/changelog/index.js
@@ -6,20 +6,23 @@ const { resolve } = require('path');
 const known = require('./commits.json');
 
 const TITLES = [
-	{ title: 'Features', tags: ['+', 'new', 'feature'] },
-	{ title: 'Fixes', tags: ['-', 'fix', 'fixes'] },
+	{ title: 'Features', tags: ['+', 'new', 'feature', 'feat'] },
 	{ title: 'Changes', tags: ['*', 'change'] },
+	{ title: 'Fixes', tags: ['-', 'fix', 'fixes'] },
 	{ title: 'Performance Improvements', tags: ['perf', 'performance'] },
-	{ title: 'Refactoring', tags: ['!', 'refactor', 'refactoring'] },
+	{ title: 'Security', tags: ['security'] },
 	{ title: 'Documentation', tags: ['doc', 'docs'] },
 	{ title: 'Tests', tags: ['test', 'tests'] },
 	{ title: 'Build System', tags: ['build', 'ci'] },
-	{ title: 'Reverts', tags: ['reverts'] }
+	{ title: 'Reverts', tags: ['reverts', 'revert'] },
+	{ title: 'Internal Fixes', tags: ['!', 'refactor', 'refactoring', 'internal fix', 'release fix', 'housekeeping', 'chore', 'revert'] }
 ];
 
+/**
+ * @param {Record<string, any>} commit
+ */
 function transform(commit) {
 	if (known[commit.hash])
-		 
 		commit = { ...commit, ...known[commit.hash] };
 	if (!commit.tag)
 		return undefined;
@@ -27,34 +30,39 @@ function transform(commit) {
 	let { tag, message } = commit;
 
 	if (commit.revert)
-		tag = 'Revert';
+		tag = 'revert';
+
+	const changelogSection = TITLES.find(t => t.tags.includes(tag.toLowerCase()));
+	if (!changelogSection)
+		return undefined;
 
 	if (message)
 		message = message[0].toUpperCase() + message.substr(1);
 
-	const matchingTitle = TITLES.find(t => t.tags.includes(tag.toLowerCase()));
-	if (matchingTitle)
-		tag = matchingTitle.title;
-	else
-		tag = 'Changes';
-
 	return {
 		...commit,
-		tag,
+		tag: changelogSection.title,
 		message,
 		shortHash: commit.hash.substring(0, 7)
 	};
 }
 
+/**
+ * @param {{ title: string}} a
+ * @param {{ title: string}} b
+ */
 function commitGroupsSort(a, b) {
 	const gRankA = TITLES.findIndex(t => t.title === a.title);
 	const gRankB = TITLES.findIndex(t => t.title === b.title);
 	return gRankA - gRankB;
 }
 
+/**
+ * @param {Function} cb
+ */
 async function presetOpts(cb) {
 	const parserOpts = {
-		headerPattern: /^(\w*):\s*(.*)$/, // /^(\w*:|[+\-*!])\s*(.*)$/,
+		headerPattern: /^([^:]*):\s*(.*)$/, // /^(\w*:|[+\-*!])\s*(.*)$/,
 		headerCorrespondence: [
 			'tag',
 			'message'
