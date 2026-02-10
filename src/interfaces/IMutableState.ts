4cde8f205aba44f3d7ab7c24ee0c0941ec2f150d
Stanislav Natalenko <stas@natalenko.com>
2026-02-10 17:37:56 +0000

    Fix merge

--- src/interfaces/IMutableState.ts
+++ src/interfaces/IMutableState.ts
@@ -3,7 +3,15 @@ import type { IEvent } from './IEvent';
 export interface IMutableState {
 
 	/**
-	 * Apply a single event to mutate the state
+	 * Optional list of event types that are required to restore this state.
+	 *
+	 * Exposed by AbstractAggregate as `restoresFrom` and may be used by the command handler
+	 * to load only the state-relevant events when rehydrating an aggregate.
+	 */
+	handles?: Readonly<string[]>;
+
+	/**
+	 * Apply a single event to mutate the state.
 	 */
 	mutate(event: IEvent): void;
 }
